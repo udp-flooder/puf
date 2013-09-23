cb56ee1aabebbab2f69cb84491bd3a17e135d72e
udp-flooder <flooder@intrudex.net>
2013-09-23 05:22:29 +0200

    Update puf.php

--- puf.php
+++ puf.php
@@ -7,7 +7,7 @@ https://github.com/udp-flooder/puf
 
 The MIT License (MIT)
 
-Copyright (c) 2013 udp-flooder
+Copyleft (c) 2013 Phate ~ github.com/udp-flooder/
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
